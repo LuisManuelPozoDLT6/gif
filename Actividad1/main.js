c78afb6ca16f920f6572d5519dac346880ff3653
LuisManuelPozoDLT6 <83984751+LuisManuelPozoDLT6@users.noreply.github.com>
2022-01-14 01:19:37 -0600

    Add files via upload

--- Actividad1/main.js
+++ Actividad1/main.js
@@ -3,9 +3,11 @@
 const getGif = async  () => {
     let api_key = document.getElementById("apikey").value;
     let tag = document.getElementById("tag").value;
-    const URL = "https://api.giphy.com/v1/gifs/random?tag=" + tag +"&api_key="+ api_key;
+    const URL = `https://api.giphy.com/v1/gifs/random?api_key=${api_key}&tag=${tag}`;
     const response = await fetch(URL);
     const data = await response.json();
     console.log(response);
     console.log(data.data);
+    document.getElementById("gif").innerHTML = 
+    "<img src='"+data.data.images.original.url+"'>";
 }
